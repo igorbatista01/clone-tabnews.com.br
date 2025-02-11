b1a4a0e66af012db0016cb6b906d7727281b2225
Igor Batista <84692350+igorbatista01@users.noreply.github.com>
2025-02-11 23:22:42 +0000

    Feature: Salves

---
+++ pages/salves/rodrigo.js
@@ -0,0 +1,30 @@
+function Rodrigo() {
+    return (
+        <div style={{ maxWidth: "600px", margin: "auto", padding: "20px", fontFamily: "Arial, sans-serif", lineHeight: "1.6", textAlign: "justify" }}>
+            <header style={{ textAlign: "center", marginBottom: "20px" }}>
+                <h1 style={{ color: "#d9534f" }}>Salve Rodrigo</h1>
+                <p><em>Como estas?</em></p>
+            </header>
+
+            <section>
+                <p>Seguinte,</p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget quam nibh. Praesent ligula velit, mattis rhoncus tellus vitae, vehi
+                </p>
+                <p>
+                cula malesuada nibh. Integer ac egestas elit.
+                </p>
+                <p>
+                rtor id sapien molestie eleifend non eget turpis. 
+                </p>
+            </section>
+
+            <footer style={{ textAlign: "right", marginTop: "30px" }}>
+                <p>Maecenas tempor feugiat lacus, id luctus mi interdum in. Donec vel sem nibh. Sed in egestas ex. Curabitur condimentum, odio non consequat iaculis, lorem elit pulvinar justo, in cursus odio ligula et lectus.</p>
+                <p><strong> Sed ut erat sit amet odio tincidunt volutpat. Sed hendrerit ac dui ut convallis. Vestibulum suscipit rutrum ipsum ut ultrices. Donec luctus lorem</strong></p>
+            </footer>
+        </div>
+    );
+}
+
+export default Rodrigo;
