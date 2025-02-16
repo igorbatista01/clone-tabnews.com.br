0e70e325e87f64b1bdbe33625f5cbf67dfa8814a
Igor Batista <84692350+igorbatista01@users.noreply.github.com>
2025-02-16 02:32:58 +0000

    update: index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu finjo que não percebo, mas tudo está sendo observado, o esperto se faz de bobo pra ver até onde o burro se faz de inteligente. Neste jogo sutil, cada movimento é estratégico, e cada palavra tem um significado oculto. Por trás do meu sorriso sereno, estou atento a cada detalhe, absorvendo informações e compreendendo motivações. Aprendi que nem sempre é prudente revelar todas as cartas que tenho na manga, pois o conhecimento é poder, e a paciência é uma virtude.</h1>
+    return <h1>Por enquanto, nada... cri, cri, cri...</h1>
 }
 
 export default Home;
