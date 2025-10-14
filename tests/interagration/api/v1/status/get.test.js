e0f2f25787c71c4dd56607420adda7d6e7b30f98
Igor Batista <84692350+igorbatista01@users.noreply.github.com>
2025-10-14 00:33:32 +0000

    Dia 20 Aula 01

--- tests/interagration/api/v1/status/get.test.js
+++ tests/interagration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("should be a fine status result in /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
 });
