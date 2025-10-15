8efa5812899306a326bd19d2c447c7f4634e6924
Igor Batista <84692350+igorbatista01@users.noreply.github.com>
2025-10-15 01:39:16 +0000

    Dia 20 aula 04

--- tests/interagration/api/v1/status/get.test.js
+++ tests/interagration/api/v1/status/get.test.js
@@ -3,10 +3,11 @@ test("should be a fine status result in /api/v1/status", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
